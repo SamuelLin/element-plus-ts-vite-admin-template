e0cb66dc6800c863baace7ee4dce7ca46498bb2c
Samuel Lin <samuelhsnu@gmail.com>
2022-06-28 17:15:31 +0800

    feat: base router setting

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+// TODO: i18n
+import en from 'element-plus/es/locale/lang/en'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+import zhTw from 'element-plus/lib/locale/lang/zh-tw'
+
+const locale = zhTw
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <el-config-provider :locale="locale">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
